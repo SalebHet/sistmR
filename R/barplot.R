3d1c980df7e3d7a7d50408af2b181f6e4b6bba07
cneresta <clement.nerestan@inria.fr>
2023-01-16 15:57:32 +0100

    fix awfull barplot thx to Benjamin

--- R/barplot.R
+++ R/barplot.R
@@ -4,7 +4,7 @@
 
 barplot <-  function(datadf,x_var,y_var,group = "NONE",color = "RdGy",xscale = "none",yscale = "none"){
   if(group == "NONE"){
-  plot <- ggpubr::ggbarplot(datadf,x = x_var,y = y_var,fill = x_var)
+  plot <- ggpubr::ggbarplot(datadf,x = x_var,y = y_var,fill = x_var,position = position_dodge())
   }else{
     datadf[,group] <- as.factor(datadf[,group])
     plot <- ggpubr::ggbarplot(datadf,x = x_var,y = y_var,fill = group,sort.by.groups = FALSE, combine = TRUE)
